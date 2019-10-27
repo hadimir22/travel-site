51edf4f6138443d1d37f2976499234257b8cbe3d
hadimir22 <hadi.mir@goals101.in>
2019-10-27 12:35:10 +0530

    fix: separeate components created for gallery and overview

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { OverviewComponent } from './components/overview/overview.component';
+import { GaleryComponent } from './components/galery/galery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     HeaderComponent,
-    SidebarComponent
+    SidebarComponent,
+    OverviewComponent,
+    GaleryComponent
   ],
   imports: [
     BrowserModule,
